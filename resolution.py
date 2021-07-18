5a80770fccd8419f7a2b05ef377f351190e07085
Parth Mangtani <impmmmm@gmail.com>
2021-07-18 22:14:25 +0530

    initial commit

---
+++ resolution.py
@@ -0,0 +1,12 @@
+from PIL import Image
+import random
+width = 1920
+height = 1080
+im = Image.new("RGB", (width, height), "#000000")
+pixels = im.load()
+for i in range(0, width):
+    for j in range(0, height):
+        pixels[i, j] = (random.randint(0, 255), random.randint(
+            0, 255), random.randint(0, 255))
+im.show()
+im.save("resolution.png")
