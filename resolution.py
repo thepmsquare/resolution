9035f7df5a0e839c606f1687139702714c8f1112
Parth Mangtani <impmmmm@gmail.com>
2021-07-28 20:06:14 +0530

    refactor

--- resolution.py
+++ resolution.py
@@ -1,12 +1,30 @@
 from PIL import Image
 import random
-width = 1920
-height = 1080
-im = Image.new("RGB", (width, height), "#000000")
-pixels = im.load()
-for i in range(0, width):
-    for j in range(0, height):
-        pixels[i, j] = (random.randint(0, 255), random.randint(
-            0, 255), random.randint(0, 255))
-im.show()
-im.save("resolution.tiff")
+
+default_width = 1920
+default_height = 1080
+
+
+def create_image(width=default_width, height=default_height):
+
+    im = Image.new("RGB", (width, height), "#000000")
+    pixels = im.load()
+    for i in range(0, width):
+        for j in range(0, height):
+            pixels[i, j] = (random.randint(0, 255), random.randint(
+                0, 255), random.randint(0, 255))
+    im.show()
+    im.save("resolution.tiff")
+    print("Image created.")
+
+
+if __name__ == "__main__":
+    try:
+        width = int(input("Width: "))
+        height = int(input("Height: "))
+        create_image(width, height)
+    except:
+        print(
+            f"Incorrect input, generating image with default values. Width = {default_width}, Height = {default_height}.")
+        create_image()
+    pass
